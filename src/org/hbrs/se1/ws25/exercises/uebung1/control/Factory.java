bc5a705baf59cc7e05bfa5ee8d825bf2b69671f7
Leonard <leonardgisella20@gmail.com>
2025-10-04 11:30:27 +0200

    First Test-Commit

---
+++ src/org/hbrs/se1/ws25/exercises/uebung1/control/Factory.java
@@ -0,0 +1,9 @@
+package org.hbrs.se1.ws25.exercises.uebung1.control;
+
+public class Factory {
+
+    // Das ist ein Test für GitHub
+    public int test(int a, int b){
+        return a+b;
+    }
+}
